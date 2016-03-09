1999c58cb998a20f78ad77c08040703387824b4f
Gravish, Nick <nickgravish@Nicks-iMac.local>
2016-03-09 16:12:17 -0500

    generatelayup and pin align beginnings

---
+++ popupcad_manufacturing_plugins/manufacturing/pinalignsheet.py
@@ -0,0 +1,27 @@
+# # -*- coding: utf-8 -*-
+# """
+#
+#
+# Please see LICENSE for full license.
+# """
+#
+# import popupcad
+# from popupcad.manufacturing.multivalueoperation3 import MultiValueOperation3
+#
+# class PinAlignSheet(Operation2):
+#
+#     # largely copied from OuterSheet
+#     name = 'Pin alignment sheet'
+#     show = []
+#     valuenames = ['Buffer']
+#     defaults = [0.]
+#
+#     # default sheet size
+#     sheet_width = 25        # mm
+#     hole_offset = 3
+#     hole_rad    = 0.79      # alignment pin geoms
+#     cross_len   = 1         # tick length
+#     cross_horiz = 10        # horizontal dimension from center crosshair
+#     dt          = 0.001     # small thickness for crosshair
+#
+#
